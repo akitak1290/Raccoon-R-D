2d17b882f10180fef4cfa7c43dd5e3f2a52282a9
MortredN <123havietbach@gmail.com>
2024-03-15 13:42:53 -0400

    lambda - calculate scores: fix

--- lambda/calculateScores/index.mjs
+++ lambda/calculateScores/index.mjs
@@ -4,7 +4,16 @@ import getTransitScore from './transitScore.mjs'
 import getPlaceTypeScore from './placeTypeScore.mjs'
 
 export const handler = async (event) => {
-  const { origin, destination, searchRadius } = event
+  let origin = event?.origin
+  let destination = event?.destination
+  let searchRadius = event?.searchRadius
+  if (event.body && event.body !== '') {
+    const body = JSON.parse(event.body)
+    origin = body?.origin
+    destination = body?.destination
+    searchRadius = body?.searchRadius
+  }
+
   if (!origin || !destination) {
     return {
       statusCode: 400,
@@ -17,7 +26,12 @@ export const handler = async (event) => {
   const restaurantScore = await getPlaceTypeScore(origin, 'restaurant', searchRadius ?? 1500)
   const quietScore = await getPlaceTypeScore(origin, 'quiet', searchRadius ?? 1500)
 
-  if (transitScore == null || groceryScore == null || restaurantScore == null || quietScore == null) {
+  if (
+    transitScore == null ||
+    groceryScore == null ||
+    restaurantScore == null ||
+    quietScore == null
+  ) {
     return {
       statusCode: 400,
       message: 'Something goes wrong, please try again.'
